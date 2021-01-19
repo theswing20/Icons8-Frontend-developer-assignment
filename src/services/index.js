c99d38713081cc3e3764a13a72ac738de51eb148
Dean <theswing20@gmail.com>
2021-01-19 22:28:36 +0600

    Created services

---
+++ src/services/index.js
@@ -0,0 +1,20 @@
+export default function recursiveSearch (arr) {
+        let result = []
+
+        arr.$children.forEach(item => {
+            result.push(item)
+
+            if (item.$children.length) {
+                const children = recursiveSearch(item)
+
+                children.forEach(item => {
+                    if (typeof item === "object") {
+                        result.push(item)
+                    }
+                })
+            }
+        })
+
+        return result
+}
+
