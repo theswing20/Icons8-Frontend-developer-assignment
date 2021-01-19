0788bee898653639a002fce5afa0539cd5c7f534
Dean <theswing20@gmail.com>
2021-01-19 21:41:54 +0600

    transfer code from app to Home.vue

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,11 @@
 <template>
   <div id="app">
-    <BasicWrapper>
-      <BasicHeading title="Icons8 Frontend developer assignment" />
-
-      <ComponentWithFoo>
-        1
-        <OtherComponent>
-          Random component
-          <ComponentWithFoo>
-            2
-            <ComponentWithFoo>
-              3
-            </ComponentWithFoo>
-          </ComponentWithFoo>
-        </OtherComponent>
-        <ComponentWithFoo>
-          4
-        </ComponentWithFoo>
-        <ComponentWithFoo>
-          5
-        </ComponentWithFoo>
-      </ComponentWithFoo>
-    </BasicWrapper>
-
     <router-view/>
   </div>
 </template>
 
 <script>
-  import ComponentWithFoo from "./components/ComponentWithFoo";
-  import OtherComponent from "./components/OtherComponent";
-  import BasicHeading from "./components/basicComponents/BasicHeading"
-  import BasicWrapper from "./components/basicComponents/BasicWrapper"
   export default {
-    name: 'Home',
-    components: {
-      ComponentWithFoo,
-      OtherComponent,
-      BasicHeading,
-      BasicWrapper
-    },
+
   }
 </script>
